df607491afa133da9ff345ae553f1191e360ee46
cartory <xXcartoryXx@gmail.com>
2022-06-06 15:25:52 -0400

    update server > with socket-client

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 require('dotenv').config()
 
-const http = require('http')
-const socket = require('socket.io')
+const { io } = require('socket.io-client')
 
 const app = require('./src/app')
-
-const server = http.createServer(app)
-const io = new socket.Server(server)
-
 const ingredientService = require('./src/services/ingredient.service')
 
-io.on('connection', (socket) => {
-	socket.on('recipe-request', async data => {
-		const { clientId, requestState, recipe } = data
-
-		await socket.join(clientId)
-		
-		if (requestState === "preparing-recipe") {
-			const { ingredients = [] } = recipe
-
-			try {
-				ingredients.forEach(async ({ name, count }) => {
-					await ingredientService.getIngredientCount(name, count)
-				});
-
-				data.requestState = "cooking-recipe"
-				io.sockets.in(clientId).emit("recipe-response", data)
-			} catch (err) {
-				console.error(err);
-				data.requestState = "error-recipe"
-				io.sockets.in(clientId).emit("recipe-response")
-			}
-		}
-	})
+const socket = io('ws://localhost:3000', {
+	transports: ['websocket']
 })
 
-server.listen(process.env.PORT, () => {
+socket.on("recipe-request", data => {
+	const { clientId, requestState, recipe } = data
+
+	await socket.join(clientId)
+
+	if (requestState === "preparing-recipe") {
+		const { ingredients = [] } = recipe
+
+		try {
+			ingredients.forEach(async ({ name, count }) => {
+				await ingredientService.getIngredientCount(name, count)
+			});
+
+			data.requestState = "cooking-recipe"
+			socket.emit("recipe-response", data)
+		} catch (err) {
+			console.error(err);
+			data.requestState = "error-recipe"
+			socket.emit("recipe-response", data)
+		}
+	}
+})
+
+app.listen(() => {
 	console.log(new Date())
 	console.log(`Server running on \x1b[33mhttp://${process.env.HOST}:${process.env.PORT}\x1b[0m`)
 })
