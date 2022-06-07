438a4b5beed73b183ced033940772d954fbddb2b
cartory <xXcartoryXx@gmail.com>
2022-06-06 23:30:43 -0400

    update sequelize.js

--- src/settings/sequelize.js
+++ src/settings/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DATABASE_URL || "",
+    process.env.DATABASE_URL,
     {
         logging: true,
         define: {
